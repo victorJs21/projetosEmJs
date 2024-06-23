7a69bcea5574aae5510f3af5eb1bfab26f03fbc3
victorJs21 <victorjvhbo@gmail.com>
2024-06-23 19:11:48 -0300

    subindo projeto de Js

---
+++ modelo/scri.js
@@ -0,0 +1,17 @@
+var m;
+var n1 = 9;
+var n2 =7;
+var n3 = 2;
+
+m=n1;
+if (n2>m){
+ m=n2;
+}
+ if(n2>m){
+ m=n3;
+ }
+if((n1==n2)&& (n2==n3)){
+console.log('combinação')
+}else{ 
+console.log(m)
+}
